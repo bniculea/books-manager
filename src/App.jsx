204ef5c62b05a7842513456259d98aea85f9c3c0
niculeasaroxana <roxana.tilea@outlook.com>
2026-07-19 15:11:31 +0300

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <p>Main Page</p>
+    </>
+  )
+}
+
+export default App
